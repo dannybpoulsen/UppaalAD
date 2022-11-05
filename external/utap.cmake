f426f429b1cc7785bbda18747bdad99a700771b9
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2022-11-05 22:11:59 +0100

    Arrays seems functional now

--- external/utap.cmake
+++ external/utap.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
   utap
   GIT_REPOSITORY git@github.com:dannybpoulsen/utap.git
-  GIT_TAG  5e30a9c288f3345fc2abb4ec55f2e31efdfae697
+  GIT_TAG  b7ccc899129f24ab7302e6c031c13ebdec050e9f
   PATCH_COMMAND sed -i "s/2.9.14 REQUIRED//" ${CMAKE_CURRENT_BINARY_DIR}/_deps/utap-src/CMakeLists.txt
 )
 
